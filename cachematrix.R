13ea311240813ec9630269f216414d503f807287
yichengli1 <yichengl@email.unc.edu>
2015-03-22 07:39:08 -0400

    it works

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## this is my code
-## second line
-# my experience with github
+## This function returns four methods: getVal, setVal, getInv, setInv, 
 
 makeCacheMatrix <- function(x = matrix()) {
-  
+  Inv <- NULL
+  setVal <- function(inVal) {
+    x <<- inVal
+    Inv <<- NULL
+  }
+  getVal <- function() x
+  setInv <- function(inverse) Inv <<- inverse
+  getInv <- function() Inv
+  list(setVal=setVal, getVal=getVal, setInv=setInv, getInv=getInv)
 }
 
-
-## Write a short comment describing this function
+## This function will retunr the inverse of a square and inversable matrix
+## It uses "solve" function internally to do the work
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if(!is.null(inv)) {
+    message("return cached value.")
+    return(inv)
+  }
+  dt <- x$getVal()
+  inv <- solve(dt)
+  x$setInv(inv)
+  inv
 }
